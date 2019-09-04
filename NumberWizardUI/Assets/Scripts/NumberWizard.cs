579ef3e46c74b7a52d195a27606a5982ba335132
aomand <aomand@hotmail.co.uk>
2019-09-04 07:57:52 +0100

    added NumberConsoleScript

---
+++ NumberWizardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        max = max + 1;
+        guess = (max + min) / 2;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    private void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
